385d33f01cce991dc30e2e81dd9ef27497f18cb7
warren <warren.weckesser@gmail.com>
2022-12-03 12:56:56 -0500

    Add more options to dists_that_override.py

--- dists_that_override.py
+++ dists_that_override.py
@@ -1,4 +1,5 @@
-import sys
+import argparse
+import math
 from shutil import get_terminal_size
 import scipy
 from scipy.stats import _continuous_distns, rv_continuous
@@ -29,40 +30,68 @@ def overrides(name, target):
     return instance_method != class_method
 
 
-if len(sys.argv) < 2:
-    print(f"use: {sys.argv[0]} method_name [method_name2 method_name3 ...]")
-    sys.exit(-1)
+def main():
+    parser = argparse.ArgumentParser(
+        prog='dists_that_override',
+        description=('Show which SciPy continuous univariate distributions '
+                     'override methods'),
+    )
+    parser.add_argument('-s', '--support', action='store_true',
+                        help=('show the support of the distribution in the '
+                              'output'))
+    parser.add_argument('-i', '--infinite-support', action='store_true',
+                        help=('show only distributions that have infinite '
+                              'support'))
+    parser.add_argument('method', type=str, nargs='+', help='methods to check')
+    args = parser.parse_args()
 
-print(f'SciPy version {scipy.__version__}')
-print()
+    print(f'SciPy version {scipy.__version__}')
+    print()
 
-dist_names = [name for name in dir(_continuous_distns)
-              if isinstance(getattr(_continuous_distns, name), rv_continuous)]
-
-if len(sys.argv) == 2:
-    target = sys.argv[1]
     dist_names = [name for name in dir(_continuous_distns)
                   if isinstance(getattr(_continuous_distns, name),
                                 rv_continuous)]
-    with_override = [name for name in dist_names if overrides(name, target)]
-    without_override = sorted(set(dist_names) - set(with_override))
 
-    print(f"Continuous univariate distributions that override {target}:")
-    print_names(with_override)
+    if len(args.method) == 1:
+        target = args.method[0]
+        dist_names = [name for name in dir(_continuous_distns)
+                      if isinstance(getattr(_continuous_distns, name),
+                                    rv_continuous)]
+        with_override = [name for name in dist_names
+                         if overrides(name, target)]
+        without_override = sorted(set(dist_names) - set(with_override))
 
-    print()
-    print("Continuous univariate distributions that do not override "
-          f"{target}:")
-    print_names(without_override)
-else:
-    # More than one argument on the command line.
-    # Make a table with checkboxes for the methods that are overridden.
-    print(f'{"distribution":21s}', end='')
-    for method in sys.argv[1:]:
-        print(f'{method:5}', end='')
-    print()
-    for name in dist_names:
-        print(f'{name:21s}', end='')
-        for method in sys.argv[1:]:
-            print(f'{"☑" if overrides(name, method) else "☐":5}', end='')
+        print(f"Continuous univariate distributions that override {target}:")
+        print_names(with_override)
+
         print()
+        print("Continuous univariate distributions that do not override "
+              f"{target}:")
+        print_names(without_override)
+    else:
+        # More than one argument on the command line.
+        # Make a table with checkboxes for the methods that are overridden.
+        if args.infinite_support:
+            print('Showing only distributions with infinite support.\n')
+        print(f'{"distribution":21s}', end='')
+        if args.support:
+            print(f'{"support":16s}  ', end='')
+        for method in args.method:
+            print(f'{method:5}', end='')
+        print()
+        for name in dist_names:
+            dist = getattr(scipy.stats, name)
+            if (args.infinite_support and
+                    math.inf not in [abs(dist.a), abs(dist.b)]):
+                continue
+            print(f'{name:21s}', end='')
+            if args.support:
+                # Show the default support.
+                print(f'[{dist.a:6.3g}, {dist.b:6.3g}]   ', end='')
+            for method in args.method:
+                print(f'{"☑" if overrides(name, method) else "☐":5}', end='')
+            print()
+
+
+if __name__ == "__main__":
+    main()
