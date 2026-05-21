d446798c8819d5c33cfd87bbd6ef4ecb5e3c8096
Warren Weckesser <warren.weckesser@gmail.com>
2026-05-21 19:34:03 -0400

    Include count of distributions in the output of find_distributions_missing_refs.py.

--- find_distributions_missing_refs.py
+++ find_distributions_missing_refs.py
@@ -12,11 +12,19 @@ print(f"scipy version {scipy.__version__}")
 print()
 print("Distributions missing 'References' in their docstring:")
 
+
 def check_dist(name):
     obj = getattr(stats, name)
     if isinstance(obj, rv_generic) or name in _multivariate.__all__:
         if "References" not in obj.__doc__:
             print(f"    {name}")
+            return 1
+    return 0
 
+
+count = 0
 for name in dir(stats):
-    check_dist(name)
+    count += check_dist(name)
+
+print()
+print(f"Found {count} distributions without the References section.")
